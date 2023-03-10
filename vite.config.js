c706ed193b2a6e4be455a6fed38e08bdad7b470a
Talibat 3ilm <omnia.hefzy@gmail.com>
2023-03-10 21:55:58 +0300

    deploy the project

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react-intro-section-with-drop-down-navigation-menu/' ,
   plugins: [react()],
 })
